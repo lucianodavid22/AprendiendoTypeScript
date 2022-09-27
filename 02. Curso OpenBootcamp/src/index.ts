1db7837427203e7392562b48965001052ec096fd
Luciano David Dorregaray <lucianodavid2201@gmail.com>
2022-09-27 10:12:19 -0300

    Agregando avances en OpenBootcamp

--- 02. Curso OpenBootcamp/src/index.ts
+++ 02. Curso OpenBootcamp/src/index.ts
@@ -1,2 +1,23 @@
 console.log("TypeScript en OpenBootcamp");
 console.log("Hola");
+
+// Variables
+let nombre: string = "Luciano";
+
+console.log("Hola, " + nombre);
+
+let a: string, b: number, c: boolean;
+
+// Arrays
+let nombres: string[] = ["Luciano", "David"];
+let valores: (string | number | boolean)[] = [false, true, 22, "Luciano", 7];
+
+// Enum
+
+enum Estados {
+    "Completado" = 1,
+    "Incompleto",//2
+    "Pendiente"  //3
+};
+
+let estadoTarea: Estados = Estados.Pendiente;
